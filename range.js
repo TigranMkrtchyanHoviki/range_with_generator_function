42f54b4fcd2750831ea5f7ed980b20c6c4a5b078
TigranMkrtchyanHoviki <tigranmkrtchyanhoviki@gmail.com>
2023-05-06 14:36:20 +0400

    added = symbol in i < end expraetion -> i <= end

--- range.js
+++ range.js
@@ -1,10 +1,19 @@
 function* range (start, end, range) {
-    for(let i = start; i < end; i+=range) {
+    for(let i = start; i <= end; i+=range) {
         yield i
     }
 }
 
-let iter = range(0, 500, 9)
+let iter = range(0, 100, 5)
+
+// let objIter = iter.next()
+
+// while(objIter.done === false) {
+//     console.log(objIter.value)
+//     objIter = iter.next()
+// }
+
+// 10 - 15 տողերի միջակայքում գրվածը նույնպես ճիշտ ա, բայց քանի որ generator function-նը վերադարձնում է iterator object, նշանակում է, որ այդ օբյկեկտի վրա կարող ենք for of ցիկլը ֆռալ
 
 for(val of iter) {
     console.log(val)
